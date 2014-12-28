b8c4f97e688cf12d5c5e2db49df7df0fa12fbd32
Adrian Medina <adrian.medina@mail.yu.edu>
2014-12-28 17:54:19 -0500

    define generic NettyServer component

--- src/genesis/netty.clj
+++ src/genesis/netty.clj
@@ -13,4 +13,53 @@
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 (ns genesis.netty
-  )
+  (:require [com.stuartsierra.component :as c])
+  (:import [io.netty.bootstrap Bootstrap ServerBootstrap]
+           [io.netty.channel ChannelFuture ChannelInitializer ChannelOption]
+           [io.netty.channel ChannelPipeline EventLoopGroup]
+           [io.netty.channel.nio NioEventLoopGroup]
+           [io.netty.channel.socket SocketChannel]
+           [io.netty.channel.socket.nio NioServerSocketChannel]
+           [io.netty.channel.socket.nio NioDatagramChannel]
+           [io.netty.handler.logging LogLevel LoggingHandler]
+           [io.netty.handler.ssl SslContext]
+           [io.netty.handler.ssl.util SelfSignedCertificate]))
+
+(defn- default-options
+  []
+  {ChannelOption/SO_BACKLOG 100})
+
+(defn setup-tcp-server
+  [server]
+  (let [boss-group (NioEventLoopGroup.)
+        worker-group (NioEventLoopGroup.)]
+    (assoc server
+      :bootstrap (doto (Bootstrap.)
+                   (.group boss-group worker-group)
+                   (.channel NioServerSocketChannel)))))
+
+(defn setup-udp-server
+  [server]
+  (let [group (NioEventLoopGroup.)]
+    (assoc server
+      :bootstrap (doto (Bootstrap.)
+                   (.group group)
+                   (.channel NioDatagramChannel)))))
+
+(defrecord NettyServer [host port protocol handler bootstrap options]
+  c/Lifecycle
+  (start [this]
+    (let [server (doto (case protocol
+                         :udp (setup-udp-server this)
+                         (setup-tcp-server this))
+                   (.handler handler))]
+      (doseq [[k v] (merge (default-options) options)]
+        (.option (:bootstrap server) k v))
+      server)
+    this)
+  (stop [this]
+    this))
+
+(defn netty-server
+  [config]
+  (map->NettyServer config))
