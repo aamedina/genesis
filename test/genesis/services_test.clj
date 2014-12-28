5395efd12a9505243d9a261a41b2ec0fdbff9a94
Adrian Medina <adrian.medina@mail.yu.edu>
2014-12-28 17:06:27 -0500

    toy with service/command definition and extension syntax

--- test/genesis/services_test.clj
+++ test/genesis/services_test.clj
@@ -1,2 +1,36 @@
 (ns genesis.services-test
-  (:require [genesis.services :refer :all]))
+  (:require [genesis.services :refer :all])
+  (:import java.net.InetSocketAddress))
+
+(comment
+  (defremote some-udp-server
+    {:host "127.0.0.1"
+     :port 9090
+     :protocol :udp})
+
+  (defremote some-tcp-server
+    {:host "127.0.0.1"
+     :port 9091
+     :protocol :tcp})
+
+  (defservice basic-service
+    :load-balancer false
+    :cache false
+    :store false)
+
+  (def echo-service
+    (extend-service basic-service
+      :remote some-tcp-server))
+  
+  (defcommand echo
+    :service echo-service
+    identity)
+
+  (def some-udp-service
+    (extend-service basic-service
+      :remote some-tcp-server))
+
+  (defcommand ping
+    :service some-udp-service
+    
+    (constantly "pong")))
