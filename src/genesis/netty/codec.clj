18a07938e5c2b29f180806d0f312921c70f77e7d
Adrian Medina <adrian.medina@mail.yu.edu>
2015-01-01 20:46:47 -0500

    begin byte-to-message-decoder impl

--- src/genesis/netty/codec.clj
+++ src/genesis/netty/codec.clj
@@ -16,15 +16,72 @@
   (:require [genesis.netty.channels :refer [channel-handler]]
             [clojure.tools.logging :as log])
   (:import [io.netty.channel ChannelHandler]
-           [io.netty.buffer ByteBuf Unpooled]))
+           [io.netty.buffer ByteBuf Unpooled]
+           [io.netty.handler.codec DecoderException]
+           [io.netty.util.internal RecyclableArrayList StringUtil]))
 
 (set! *warn-on-reflection* true)
 
-(defn byte-to-message-decoder
-  [decoder]
-  (let [buf (volatile! nil)]
-    (channel-handler
-     (handlerRemoved [_ ctx])
-     (channelRead [_ ctx msg])
-     (channelReadComplete [_ ctx])
-     (channelInactive [_ ctx]))))
+(defprotocol Decoder
+  (decode [_ ctx in out]))
+
+(defmacro byte-to-message-decoder
+  [decoder & specs]
+  `(let [cumulation# (volatile! nil)]
+     (channel-handler
+       (handlerRemoved [_ ctx#]
+         (when-let [^ByteBuf buf# @cumulation#]
+           (when (.isReadable buf#)
+             (.fireChannelRead ctx# (.readBytes buf# (.readableBytes buf#)))))
+         (vreset! cumulation# nil)
+         (.fireChannelReadComplete ctx#))
+       (channelRead [this# ctx# msg#]
+         (if (instance? ByteBuf msg#)
+           (let [out# (RecyclableArrayList/newInstance)]
+             (try
+               (if (nil? @cumulation#)
+                 (vreset! cumulation# msg#)
+                 (let [buf# @cumulation#]
+                   (when (or (> (.writerIndex buf#)
+                                (- (.maxCapacity buf#)
+                                   (.readableBytes msg#)))
+                             (> (.refCnt buf#) 1))
+                     (let [new# (.. ctx#
+                                    (alloc)
+                                    (buffer (+ (.readableBytes buf#)
+                                               (.readableBytes msg#))))]
+                       (.writeBytes new#)
+                       (.release buf#)
+                       (vreset! cumulation# new#)))
+                   (.writeBytes buf# data#)
+                   (.release msg#)))
+               (let [buf# @cumulation#]
+                 (loop []
+                   (when (.isReadable buf#)
+                     (let [out-size# (.size out#)
+                           old-input-len# (.readableBytes in#)]
+                       (.decode this# ctx# buf# out#))
+                     (when-not (.isRemoved ctx#)
+                       (cond
+                         (== out-size# (.size out#))
+                         (when-not (== out-input-len# (.readableBytes buf#))
+                           (recur))
+
+                         (== out-input-len# (.readableBytes buf#))
+                         (throw (DecoderException. ""))
+
+                         :else (recur))))))
+               (catch DecoderException e#
+                 (throw e#))
+               (catch Throwable e#
+                 (throw (DecoderException. e#)))
+               (finally)))
+           (.fireChannelRead ctx# msg#)))
+       (channelReadComplete [_ ctx#]
+         (.fireChannelReadComplete ctx#))
+       (channelInactive [_ ctx#])
+
+       genesis.netty.codec.Decoder
+       (decode ~decoder)
+       
+       ~@specs)))
